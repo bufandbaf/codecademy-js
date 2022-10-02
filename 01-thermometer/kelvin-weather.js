cf313c3b21bdc09a42a2c942e6f6e7534002fb48
bufandbaf <tomas@cesek.net>
2022-10-02 16:55:12 +0200

    feat: html+css+javascript document.write

--- 01-thermometer/kelvin-weather.js
+++ 01-thermometer/kelvin-weather.js
@@ -1,19 +1,21 @@
-// test
-
 // The forecast today is 293 Kelvin.
 const kelvin = 300;
 // Celsius is 273 degrees less than Kelvin. The forecast today is 20 Celsius.
 const celsius = kelvin - 273;
-// Fahrenheit is calculated as the "let fahrenheit" variable follows. The forecast today is 68 Fahrenheit. 
-let fahrenheit = celsius * (9/5) + 32;
+// Fahrenheit is calculated as the "let fahrenheit" variable follows. The forecast today is 68 Fahrenheit.
+let fahrenheit = celsius * (9 / 5) + 32;
 // The following method is to round down the temperature
 fahrenheit = Math.floor(fahrenheit);
 // Convert to Newton
-let newton = celsius * (33/100);
+let newton = celsius * (33 / 100);
 // Round down
 newton = Math.floor(newton);
 
-console.log("The temperature is " + fahrenheit + " degrees Fahrenheit.");
-console.log("The temperature is " + kelvin + " degrees Kelvin.");
-console.log("The temperature is " + celsius + " degrees Celsius.");
-console.log("The temperature is " + newton + " degrees Newton.");
+function show(text, how) {
+  document.write(`<${how ?? "h3"}>${text}</${how ?? "h3"}>`);
+}
+
+show("The temperature is " + fahrenheit + " degrees Fahrenheit.", "h4");
+show("The temperature is " + kelvin + " degrees Kelvin.");
+show("The temperature is " + celsius + " degrees Celsius.");
+show("The temperature is " + newton + " degrees Newton.");
