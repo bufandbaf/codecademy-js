21fc8545655b68eb78e3d2a000d45b04a1e4e0ce
bufandbaf <tomas@cesek.net>
2022-10-21 17:09:09 +0200

    feat: millisecond counter created

---
+++ millisecond-counter.js
@@ -0,0 +1,8 @@
+const originalTime = 604800000;
+const oneHour = 3600000;
+
+let minimumTime = originalTime - oneHour;
+let maximumTime = originalTime + oneHour;
+
+console.log(minimumTime);
+console.log(maximumTime);
