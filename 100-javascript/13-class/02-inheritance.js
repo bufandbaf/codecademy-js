67411148fb2917eae3f3c83c28244c0a66e8e6e0
bufandbaf <tomas@cesek.net>
2022-12-05 13:48:42 +0100

    feat: inheritance parent class

---
+++ 100-javascript/13-class/02-inheritance.js
@@ -0,0 +1,15 @@
+class HospitalEmployee {
+  constructor(name) {
+    this._name = name;
+    this._remainingVacationDays = 20;
+  }
+  get name() {
+    return this._name;
+  }
+  get remainingVacationDays() {
+    return this._remainingVacationDays;
+  }
+  takeVacationDays(daysOff) {
+    this._remainingVacationDays -= daysOff;
+  }
+}
