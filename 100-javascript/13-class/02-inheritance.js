221a16d46f0934e7fd8869bc46c8d1d56bc3d369
bufandbaf <tomas@cesek.net>
2022-12-05 13:57:33 +0100

    feat: inheritance child class

--- 100-javascript/13-class/02-inheritance.js
+++ 100-javascript/13-class/02-inheritance.js
@@ -3,13 +3,26 @@ class HospitalEmployee {
     this._name = name;
     this._remainingVacationDays = 20;
   }
+
   get name() {
     return this._name;
   }
+
   get remainingVacationDays() {
     return this._remainingVacationDays;
   }
+
   takeVacationDays(daysOff) {
     this._remainingVacationDays -= daysOff;
   }
 }
+
+class Nurse extends HospitalEmployee {
+  constructor(name, certifications) {
+    super(name);
+    this._name = name;
+    this._certifications = certifications;
+  }
+}
+
+const nurseOlynyk = new Nurse("Olynyk", ["Trauma", "Pediatrics"]);
