85a96c0d1b13af05fb765bd39dafacca3f59d7b4
bufandbaf <tomas@cesek.net>
2022-12-13 14:41:07 +0100

    feat: workaround import

--- 100-javascript/17-workaround-explorer/workAroundModule.js
+++ 100-javascript/17-workaround-explorer/workAroundModule.js
@@ -5,28 +5,28 @@ import salaryData from "./salaryData.js";
 
 // Replace the empty array with the appropriate imported function/value
 const getAverageSalaryByRole = (role) => {
-  const roleData = [];
+  const roleData = getDataByRole(role);
   const salariesOfRole = roleData.map((obj) => obj.salary);
   return calculateAverage(salariesOfRole);
 };
 
 // Replace the empty array with the appropriate imported function/value
 const getAverageSalaryByCompany = (company) => {
-  const companyData = [];
+  const companyData = getDataByCompany(company);
   const salariesAtCompany = companyData.map((obj) => obj.salary);
   return calculateAverage(salariesAtCompany);
 };
 
 // Replace the empty array with the appropriate imported function/value
 const getSalaryAtCompany = (role, company) => {
-  const companyData = [];
+  const companyData = getDataByCompany(company);
   const roleAtCompany = companyData.find((obj) => obj.role === role);
   return roleAtCompany.salary;
 };
 
 // Replace the empty array with the appropriate imported function/value
 const getIndustryAverageSalary = () => {
-  const allSalaries = [].map((obj) => obj.salary);
+  const allSalaries = salaryData.map((obj) => obj.salary);
   return calculateAverage(allSalaries);
 };
 
